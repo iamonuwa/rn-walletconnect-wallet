51a77da5c7421b9fd5ea40324e571fc9ecb92ad0
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-07-16 12:12:30 +0100

    Update verifiedData to null by default

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
-import { Connector, handleResponse } from 'js-walletconnect-core'
+import {Connector, handleResponse} from 'js-walletconnect-core'
 
 export default class WalletConnector extends Connector {
   //
   // send session status
   //
   async sendSessionStatus(sessionData = {}) {
-    const { fcmToken, walletWebhook, data = {} } = sessionData
+    const {
+      fcmToken,
+      walletWebhook,
+      data = {}
+    } = sessionData
     if (!fcmToken || !walletWebhook) {
       throw new Error('fcmToken and walletWebhook are required')
     }
 
-    let verifiedData
+    let verifiedData = null
     // check if provided data is array or string
     if (Array.isArray(data)) {
       data.map(address => {
-        this.validateEthereumAddress(address) ? verifiedData = data : console.log('Invalid eth address')
+        if (this.validateEthereumAddress(address)) {
+          verifiedData = data
+        } else {
+          throw new Error('Invalid ethereum address')
+        }
       })
     } else if (typeof data === 'string') {
-      verifiedData = data.split().map(address => {
-        this.validateEthereumAddress(address) ? verifiedData = data.split() : console.log('Invalid eth address')
-      })
+      verifiedData = data
+        .split()
+        .map(address => {
+          if (this.validateEthereumAddress(address)) {
+            verifiedData = data.split()
+          } else {
+            throw new Error('Invalid ethereum address')
+          }
+        })
     }
 
     // encrypt data
@@ -32,11 +46,7 @@ export default class WalletConnector extends Connector {
         Accept: 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        fcmToken,
-        walletWebhook,
-        data: encryptedData
-      })
+      body: JSON.stringify({fcmToken, walletWebhook, data: encryptedData})
     })
     handleResponse(res)
     return true
@@ -54,21 +64,14 @@ export default class WalletConnector extends Connector {
     const encryptedData = await this.encrypt(statusData)
 
     // store transaction info on bridge
-    const res = await fetch(
-      `${this.bridgeURL}/session/${
-        this.sessionId
-      }/transaction/${transactionId}/status/new`,
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          data: encryptedData
-        })
-      }
-    )
+    const res = await fetch(`${this.bridgeURL}/session/${this.sessionId}/transaction/${transactionId}/status/new`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({data: encryptedData})
+    })
     handleResponse(res)
     return true
   }
@@ -76,9 +79,8 @@ export default class WalletConnector extends Connector {
   //
   // get session request data
   //
-  // async getSessionRequest() {
-  //   return this._getEncryptedData(`/session/${this.sessionId}`)
-  // }
+  // async getSessionRequest() {   return
+  // this._getEncryptedData(`/session/${this.sessionId}`) }
 
   //
   // get transaction request data
@@ -88,9 +90,7 @@ export default class WalletConnector extends Connector {
       throw new Error('transactionId is required')
     }
 
-    return this._getEncryptedData(
-      `/session/${this.sessionId}/transaction/${transactionId}`
-    )
+    return this._getEncryptedData(`/session/${this.sessionId}/transaction/${transactionId}`)
   }
 
   async validateEthereumAddress(address) {
